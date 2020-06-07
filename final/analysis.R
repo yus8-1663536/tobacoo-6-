372c264ee4c308b60906785b790a6cdaf1e2e9ca
kohand27 <kohand27@uw.edu>
2020-06-07 11:50:00 -0700

    Added data grabbing

--- final/analysis.R
+++ final/analysis.R
@@ -1,2 +1,8 @@
 library(dplyr)
 library(ggplot2)
+library(tidyr)
+library(reshape2)
+library(scales)
+
+demo <- read.csv("data/demographic data.csv", stringsAsFactors = F)
+tobacco <- read.csv("data/tobacco.csv", stringsAsFactors = F)
